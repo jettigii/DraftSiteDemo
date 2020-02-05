da5ff64033080814bb692df8026e0e28d39e7748
jettigia <jettigia@yahoo.com>
2020-02-05 07:48:23 -0500

    Create draft view added
    Create draft view added, draft lobby template added, and style added.

---
+++ draft-site-website/src/components/DraftLobby.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="DraftLobby">
+
+    <div id="main">
+        <!-- Post -->
+        <section class="post">
+            <header class="major">
+
+            </header>
+        </section>
+    </div>
+      
+  </div>
+</template>
+
+<style scoped>
+
+
+
+</style>
